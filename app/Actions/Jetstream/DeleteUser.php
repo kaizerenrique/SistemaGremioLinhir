e2537e0c825f43f5873e28dd0d2106f88dd4d678
Oliver Gomez <kayserenrique@gmail.com>
2025-07-29 07:28:48 -0400

    eliminar informacion del proveedor

--- app/Actions/Jetstream/DeleteUser.php
+++ app/Actions/Jetstream/DeleteUser.php
@@ -14,6 +14,7 @@ class DeleteUser implements DeletesUsers
     {
         $user->deleteProfilePhoto();
         $user->tokens->each->delete();
+        $user->authProviders()->delete(); //eliminar informacion del proveedor
         $user->delete();
     }
 }
