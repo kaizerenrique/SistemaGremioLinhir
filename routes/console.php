82569e25450c662e6b33bbe5d840c8660bda88f1
Oliver Gomez <kayserenrique@gmail.com>
2025-08-03 05:02:22 -0400

    configuracion de tarea

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+
+$schedule->command('server:check')
+        ->everyFifteenSeconds()  // Ejecutar cada 15 minutos
+        ->timeBetween('9:00', '10:30') // Solo en este rango horario UTC
+        ->withoutOverlapping()   // Prevenir ejecuciones simultáneas
+        ->onOneServer();         // Para entornos con múltiples servidores
